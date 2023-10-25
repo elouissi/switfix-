b782970b43a2adadd1b4e58063ee2c08ab7eabc2
elouissi <yassinelouissi67@gmail.com>
2023-10-25 20:14:29 +0100

    terminer la tache de ala validation

--- javascript.js
+++ javascript.js
@@ -1,48 +1,132 @@
- 
+
 
 
 const burgerbtn = document.querySelector(".burgerbtn");
 const navbar = document.querySelector(".navbar-nav2")
-burgerbtn.addEventListener("click" , () => {
+burgerbtn.addEventListener("click", () => {
     navbar.classList.toggle("navbar-nav2--active")
-    
-} )
+
+})
 
 
 
 const buttonserch = document.querySelector(".button-serch");
 const btnclick = document.querySelector(".buttonclick")
-buttonserch.addEventListener("click" , () => {
+buttonserch.addEventListener("click", () => {
     btnclick.classList.toggle("buttonclick--active")
-    
-} )
-    const FAQdetails1 = document.querySelector(".remplir");
-    if(FAQdetails1){
-const details1 = FAQdetails1.querySelector(".FAQ")
-FAQdetails1.addEventListener("click" , () => {
-    details1.classList.toggle("FAQ--active")
+
+})
+
+const FAQdetails1 = document.querySelector(".remplir");
+if (FAQdetails1) {
+    const details1 = FAQdetails1.querySelector(".FAQ")
+    FAQdetails1.addEventListener("click", () => {
+        details1.classList.toggle("FAQ--active")
+    })
+
+
     const FAQdetails2 = FAQdetails1.nextElementSibling
-    const details2 = FAQdetails2.querySelector(".FAQ")
-    FAQdetails2.addEventListener("click" , () => {
-        details2.classList.toggle("FAQ--active")
-        
-    } )
-    const FAQdetails3 =  FAQdetails2.nextElementSibling
-    const details3 = FAQdetails3.querySelector(".FAQ")
-    FAQdetails3.addEventListener("click" , () => {
-        details3.classList.toggle("FAQ--active")
-        
-    } )
-    
-} )
+    if (FAQdetails2) {
+        const details2 = FAQdetails2.querySelector(".FAQ")
+        FAQdetails2.addEventListener("click", () => {
+            details2.classList.toggle("FAQ--active")
+
+        })
+    }
+
+    const FAQdetails3 = FAQdetails2.nextElementSibling
+    if (FAQdetails3) {
+        const details3 = FAQdetails3.querySelector(".FAQ")
+        FAQdetails3.addEventListener("click", () => {
+            details3.classList.toggle("FAQ--active")
+
+        })
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 const imgrdrop = document.querySelector(".imgdrop");
 const listservice = document.querySelector(".listservice")
 
-imgrdrop.addEventListener("click" , () => {
+imgrdrop.addEventListener("click", () => {
     listservice.classList.toggle("listservice--active")
     console.log(navbar);
-    
-} ) 
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function validateForm() {
+    const name = document.getElementById('name');
+    const phone = document.getElementById('phone_number');
+    const email = document.getElementById('email');
+    const message = document.getElementById('message');
+
+    if (name.value === '') {
+        document.getElementById('nameError').innerText = 'Name cannot be blank';
+        name.classList.add("input-error")
+    } else {
+        document.getElementById('nameError').innerText = '';
+        name.classList.add("input-valid")
+
+    }
+
+    const phoneRegex = /\+212\-+[6|7]+\d{8}/g;
+    if (!phoneRegex.test(phone.value)) {
+        document.getElementById('phoneError').innerText = 'Enter a valid phone number';
+        phone.classList.add("input-error");
+    } else {
+        document.getElementById('phoneError').innerText = '';
+        phone.classList.add("input-valid");
+    }
+
+
+
+    const emailRegex = /^[a-zA-Z0-9._-]+@gmail+\.[a-zA-Z]{2,4}$/;
+    if (!emailRegex.test(email.value)) {
+        document.getElementById('emailError').innerText = 'Enter a valid email';
+        email.classList.add("input-error")
+        
+    } else {
+        document.getElementById('emailError').innerText = '';
+        email.classList.add("input-valid")
+    }
+
+
+    const messageRgex = /\w$/;
+    if (!messageRgex.test(message.value)) {
+        document.getElementById('messageError').innerText = 'Message cannot be blank';
+        message.classList.add("input-error")
+    } else {
+        document.getElementById('messageError').innerText = '';
+        message.classList.add("input-valid")
+    }
+}
+
+
+
+
