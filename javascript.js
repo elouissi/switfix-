71c7a3bdef6f5f9a51033b7827883abfc7aa336c
elouissi <yassinelouissi67@gmail.com>
2023-10-25 14:57:22 +0100

    terminer U.S 4

--- javascript.js
+++ javascript.js
@@ -16,23 +16,33 @@ buttonserch.addEventListener("click" , () => {
     btnclick.classList.toggle("buttonclick--active")
     
 } )
-const FAQdetails1 = document.querySelector(".remplir");
+    const FAQdetails1 = document.querySelector(".remplir");
+    if(FAQdetails1){
 const details1 = FAQdetails1.querySelector(".FAQ")
 FAQdetails1.addEventListener("click" , () => {
     details1.classList.toggle("FAQ--active")
+    const FAQdetails2 = FAQdetails1.nextElementSibling
+    const details2 = FAQdetails2.querySelector(".FAQ")
+    FAQdetails2.addEventListener("click" , () => {
+        details2.classList.toggle("FAQ--active")
+        
+    } )
+    const FAQdetails3 =  FAQdetails2.nextElementSibling
+    const details3 = FAQdetails3.querySelector(".FAQ")
+    FAQdetails3.addEventListener("click" , () => {
+        details3.classList.toggle("FAQ--active")
+        
+    } )
     
 } )
- 
-const FAQdetails2 = FAQdetails1.nextElementSibling
-const details2 = FAQdetails2.querySelector(".FAQ")
-FAQdetails2.addEventListener("click" , () => {
-    details2.classList.toggle("FAQ--active")
+}
+
+
+const imgrdrop = document.querySelector(".imgdrop");
+const listservice = document.querySelector(".listservice")
+
+imgrdrop.addEventListener("click" , () => {
+    listservice.classList.toggle("listservice--active")
+    console.log(navbar);
     
-} )
-const FAQdetails3 =  FAQdetails2.nextElementSibling
-const details3 = FAQdetails3.querySelector(".FAQ")
-FAQdetails3.addEventListener("click" , () => {
-    details3.classList.toggle("FAQ--active")
-    
-} )
- 
+} ) 
